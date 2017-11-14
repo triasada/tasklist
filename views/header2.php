87f01ee127c1a0b9aa7f02c141250f568dec42c3
Hakim <Hakim@Hakimpc>
2017-11-15 03:47:17 +0700

    fix redirect

--- views/header2.php
+++ views/header2.php
@@ -41,8 +41,8 @@
 											  <button data-toggle="dropdown" class="btn btn-primary dropdown-toggle"><img height="20px"  src="<?php echo BACKEND_TEMPLATE; ?>assets/img/find_user.png" class="img-circle" />
                                                                                               <?php echo $this->nameuser ?> <span class="caret"></span></button>
 											  <ul class="dropdown-menu dropdown-menu-right">
-												<li><a href="<?=URL?>/user/user/edit/<?=$_SESSION['userid']?>">Profile</a></li>
-                                                                                                <li><a href="<?=URL?>/user/user/changePass/<?=$_SESSION['userid']?>" >Change Password</a></li>
+												<li><a href="<?=URL?>user/user/edit/<?=$_SESSION['userid']?>">Profile</a></li>
+                                                                                                <li><a href="<?=URL?>user/user/changePass/<?=$_SESSION['userid']?>" >Change Password</a></li>
 												<li class="divider"></li>
                                                                                                 <li><a href="javascript:void(0)" onclick="if(confirm('Sure go Out?')){ window.location.href='<?= URL ?>dashboard/dashboard/logout'}">Logout</a></li>
 											  </ul>
