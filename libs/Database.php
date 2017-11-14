6ac7d9e5b46297546e5a6d4d8a457b5f95afac2a
Hakim <Hakim@Hakimpc>
2017-11-15 03:32:50 +0700

    resolve

--- libs/Database.php
+++ libs/Database.php
@@ -7,8 +7,6 @@ class Database extends PDO {
     public function __construct($DB_TYPE, $DB_HOST, $DB_NAME, $DB_USER, $DB_PASS) {
         parent::__construct($DB_TYPE . ':host=' . $DB_HOST . ';dbname=' . $DB_NAME, $DB_USER, $DB_PASS);
         $this->log = new Logging();
-        $this->log->lfile('c:/xampp56/htdocs/ict-portal/log/logfile_' . date('dMY') . '.txt');
-        //parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTIONS);
     }
 
     /**
