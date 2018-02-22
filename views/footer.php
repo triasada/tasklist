eb5f2c44148352c5e8ce5f6b4df4c819ef26ed2b
satria.persada <satria.persada@localhost>
2018-02-22 16:21:27 +0700

    add departement to project

--- views/footer.php
+++ views/footer.php
@@ -15,7 +15,7 @@
             success: function (data) {
                 if (data.success) {
                     var newdata= data.data;
-                    $('#isikiri').append("<h3>Project List</h3><br>");
+                    $('#isikiri').append("<h4>Project List</h3><br>");
                     for (i = 0; i < newdata.length; i++) {
                         var colorijo;
                         switch (newdata[i].priority_id) {
@@ -34,10 +34,10 @@
         
         break;
 }
-                       $('#isikiri').append("<h4 class='"+colorijo+"'>"+newdata[i].project+"  ("+newdata[i].taskall+" task)</h4>");
+                       $('#isikiri').append("<h5 class='"+colorijo+"'>"+newdata[i].project+"  ("+newdata[i].taskall+" task)</h4>");
                        var percent = Math.round(100*newdata[i].taskdone/newdata[i].taskall)
                        if (!percent){percent = 0}
-                        $('#isikiri').append(" <div class='progress'><div class='progress-bar-success' role='progressbar' aria-valuenow='"+percent+"' aria-valuemin='0' aria-valuemax='100' style='width:"+percent+"%'>\n\
+                        $('#isikiri').append(" <div class='progress'><div class='progress-bar progress-bar-success' role='progressbar' aria-valuenow='"+percent+"' aria-valuemin='0' aria-valuemax='100' style='width:"+percent+"%'>\n\
     <span >"+percent+"% Complete</span> </div></div>");
                         console.log(data.data);
                     }
